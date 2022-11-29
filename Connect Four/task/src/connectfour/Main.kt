f3bec53d332778bd009fd05ac39daa7e927ce154
s7onov <sergey7onov@gmail.com>
2022-11-29 19:48:31 +0600

    Added colors

--- Connect Four/task/src/connectfour/Main.kt
+++ Connect Four/task/src/connectfour/Main.kt
@@ -7,6 +7,11 @@ const val INVALID_INPUT = "Invalid input"
 const val EMPTY_PLACE_CHAR = ' '
 const val FIRST_CHAR = 'o'
 const val SECOND_CHAR = '*'
+const val NUMBER_OF_GAMES = """
+    Do you want to play single or multiple games?
+    For a single game, input 1 or press Enter
+    Input a number of games:
+"""
 
 fun main() {
     val game = Game()
@@ -20,6 +25,10 @@ class Game {
     private var columns = 7
     private var turn = 1
     private var board = Array(rows) { Array(columns) { EMPTY_PLACE_CHAR } }
+    private var numberOfGames = 1
+    private var currentGame = 1
+    private var firstScore = 0
+    private var secondScore = 0
 
     fun startGame() {
         println(GAME_NAME)
@@ -28,24 +37,40 @@ class Game {
         println("Second player's name:")
         secondPlayer = readln()
 
-        do {
-            val isInputCorrect = readDimensions()
-        } while (!isInputCorrect)
+        do { val isInputCorrect = readDimensions() } while (!isInputCorrect)
+        do { numberOfGames = readNumberOfGames() } while (numberOfGames <= 0)
 
         println("$firstPlayer VS $secondPlayer")
         println("$rows X $columns board")
+        println(if (numberOfGames == 1) "Single game" else "Total $numberOfGames games")
         board = Array(rows) { Array(columns) { EMPTY_PLACE_CHAR } }
-        printBoard()
 
-        do {
-            val turnCode = readPlayersTurn()
-            if (turnCode > 0) {
-                printBoard()
-                if (checkForEnd()) break
-                turn++
-            }
-        } while (turnCode >= 0)
+        for (i in 1..numberOfGames) {
+            currentGame = i
+            if (numberOfGames > 1) println("Game #$i")
+            printBoard()
+            do {
+                val turnCode = readPlayersTurn()
+                if (turnCode > 0) {
+                    printBoard()
+                    if (checkForEnd()) break
+                    turn++
+                }
+            } while (turnCode >= 0)
+        }
         println("Game over!")
+    }
+
+    private fun readNumberOfGames(): Int {
+        println(NUMBER_OF_GAMES.trimIndent())
+        val input = readln().trim()
+        if (input.isEmpty()) return 1
+        var number = -1
+        try {
+            if (input.matches(Regex("""\d+"""))) number = input.toInt()
+        } catch (ex: Exception) { number = -2 }
+        if (number <= 0) println(INVALID_INPUT)
+        return number
     }
 
     private fun checkForEnd(): Boolean {
@@ -79,16 +104,22 @@ class Game {
         val firstRegex = Regex(""".*?$FIRST_CHAR{4}.*?""")
         val secondRegex = Regex(""".*?\$SECOND_CHAR{4}.*?""")
         for (str in checkList) {
-            if (str.matches(firstRegex)) { println("Player $firstPlayer won"); return true }
-            if (str.matches(secondRegex)) { println("Player $secondPlayer won"); return true }
+            if (str.matches(firstRegex)) { println("Player $firstPlayer won"); firstScore += 2; gameEnded(); return true }
+            if (str.matches(secondRegex)) { println("Player $secondPlayer won"); secondScore += 2; gameEnded(); return true }
         }
-        if (turn == rows * columns) { println("It is a draw"); return true }
+        if (turn == rows * columns) { println("It is a draw"); firstScore++; secondScore++; gameEnded(); return true }
         return false
     }
 
+    private fun gameEnded() {
+        if (numberOfGames > 1) println("Score\n$firstPlayer: $firstScore $secondPlayer: $secondScore")
+        board = Array(rows) { Array(columns) { EMPTY_PLACE_CHAR } } // clear the board
+        turn = 1
+    }
+
     private fun readPlayersTurn(): Int {
-        println(if (turn % 2 == 1) "$firstPlayer's turn:" else "$secondPlayer's turn:")
-        val input = readln().trim().lowercase()
+        println(if ((turn + currentGame) % 2 == 0) "$firstPlayer's turn:" else "$secondPlayer's turn:")
+        val input = readln().trim()
         if (input == "end") return -1
         if (!input.matches(Regex("""\d+"""))) {
             println("Incorrect column number")
@@ -104,7 +135,7 @@ class Game {
                 println("Column $column is full")
                 return 0
             } else {
-                val char = if (turn % 2 == 1) FIRST_CHAR else SECOND_CHAR
+                val char = if ((turn + currentGame) % 2 == 0) FIRST_CHAR else SECOND_CHAR
                 for (i in rows - 1 downTo 0) {
                     if (board[i][column - 1] == EMPTY_PLACE_CHAR) {
                         board[i][column - 1] = char
@@ -154,8 +185,15 @@ class Game {
 
     private fun printBoard() {
         println(Array(columns) { it + 1 }.joinToString(" ", " "))
+        //for (i in 0 until rows) println(board[i].joinToString("║", "║", "║"))
         for (i in 0 until rows) {
-            println(board[i].joinToString("║", "║", "║"))
+            print("║")
+            for (j in 0 until columns) {
+                if (board[i][j] == FIRST_CHAR) print("\u001B[48:5:9m${board[i][j]}\u001B[0m║")
+                else if (board[i][j] == SECOND_CHAR) print("\u001B[48:5:11m${board[i][j]}\u001B[0m║")
+                else print("${board[i][j]}║")
+            }
+            println()
         }
         println(Array(columns) { "═" }.joinToString("╩", "╚", "╝"))
     }
